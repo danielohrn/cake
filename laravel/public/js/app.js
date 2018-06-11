5d4fc3697f2cf807ab234807a85ce2e6fe2a1132
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2018-06-11 12:17:22 +0200

    add truncate function to body of project cards

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -16247,7 +16247,7 @@ function filterOutTags(article, allTags) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(24);
-module.exports = __webpack_require__(135);
+module.exports = __webpack_require__(140);
 
 
 /***/ }),
@@ -16299,7 +16299,7 @@ Vue.component('ProjectTile', __webpack_require__(115));
 Vue.component('ProjectCard', __webpack_require__(120));
 Vue.component('EditProjectModal', __webpack_require__(125));
 Vue.component('Sidebar', __webpack_require__(130));
-Vue.component('NewArticleModal', __webpack_require__(137));
+Vue.component('NewArticleModal', __webpack_require__(135));
 
 var app = new Vue({
   el: '#app',
@@ -43883,6 +43883,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             var regex = new RegExp('<[a-z]+>|</[a-z]+>', 'gi');
             return string.replace(regex, '');
         }
+    },
+
+    computed: {
+        truncate: function truncate() {
+            return this.project.body.substring(0, 50) + '...';
+        }
     }
 
 });
@@ -43907,7 +43913,7 @@ var render = function() {
       ]),
       _vm._v(" "),
       _c("p", { staticClass: "subtitle is-6" }, [
-        _vm._v(" " + _vm._s(_vm.trimHtmlTags(_vm.project.body)))
+        _vm._v(" " + _vm._s(_vm.trimHtmlTags(_vm.truncate)))
       ]),
       _vm._v(" "),
       _c(
@@ -44773,25 +44779,18 @@ if (false) {
 
 /***/ }),
 /* 135 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 136 */,
-/* 137 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var disposed = false
 function injectStyle (ssrContext) {
   if (disposed) return
-  __webpack_require__(138)
+  __webpack_require__(136)
 }
 var normalizeComponent = __webpack_require__(2)
 /* script */
-var __vue_script__ = __webpack_require__(140)
+var __vue_script__ = __webpack_require__(138)
 /* template */
-var __vue_template__ = __webpack_require__(141)
+var __vue_template__ = __webpack_require__(139)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
@@ -44830,13 +44829,13 @@ module.exports = Component.exports
 
 
 /***/ }),
-/* 138 */
+/* 136 */
 /***/ (function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(139);
+var content = __webpack_require__(137);
 if(typeof content === 'string') content = [[module.i, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
@@ -44856,7 +44855,7 @@ if(false) {
 }
 
 /***/ }),
-/* 139 */
+/* 137 */
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(0)(false);
@@ -44870,7 +44869,7 @@ exports.push([module.i, "\n.modal-background[data-v-7023c6a0]{\n  background-col
 
 
 /***/ }),
-/* 140 */
+/* 138 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -44926,7 +44925,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 });
 
 /***/ }),
-/* 141 */
+/* 139 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var render = function() {
@@ -45032,6 +45031,12 @@ if (false) {
     require("vue-hot-reload-api")      .rerender("data-v-7023c6a0", module.exports)
   }
 }
+
+/***/ }),
+/* 140 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
